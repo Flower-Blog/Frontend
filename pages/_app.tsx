905b5d513ea7dc02a741c9f94881572e8c0f32d1
林映如 <113336518+uinnnnnn@users.noreply.github.com>
2023-05-21 07:37:43 +0800

    fix: layout

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,11 @@
 import "@/styles/globals.css";
 
 import type { AppProps } from "next/app";
-import { Suspense } from "react";
 import { Provider } from "react-redux";
 import { store } from "store";
 
 import Navbar from "@/components/Navbar";
 
-import ErrorBoundary from "./ErrorBoundary";
 declare global {
   interface Window {
     ethereum?: any;
@@ -15,19 +13,12 @@ declare global {
 }
 
 export default function App({ Component, pageProps }: AppProps) {
-  const PageComponent = Component as React.ComponentType<any>;
-  console.log("123123");
-  console.log(`Rendering page: ${PageComponent.displayName || PageComponent.name}`);
   return (
-    <ErrorBoundary>
-      <Suspense fallback={<div>Loading...</div>}>
-        <Provider store={store}>
-          <div className="bg-threefish-or">
-            <Navbar rootClassName="navbar-root-class-name"></Navbar>
-            <Component {...pageProps} />
-          </div>
-        </Provider>
-      </Suspense>
-    </ErrorBoundary>
+    <Provider store={store}>
+      <div className="bg-threefish-or">
+        <Navbar rootClassName="navbar-root-class-name"></Navbar>
+        <Component {...pageProps} />
+      </div>
+    </Provider>
   );
 }
