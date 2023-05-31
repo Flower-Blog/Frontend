d88af0cccae4b7cc9cd1e541ab84afbdf13fc1d0
林映如 <113336518+uinnnnnn@users.noreply.github.com>
2023-06-01 06:47:05 +0800

    fix: img

--- components/home/RightSidebar.tsx
+++ components/home/RightSidebar.tsx
@@ -6,7 +6,7 @@ const RightSidebar = () => {
       <div className="home-container08">
         <div className="home-container13">
           <h1 className="my-1 text-2xl font-semibold">今日一花</h1>
-          <img alt="image" src="/playground_assets/flower11-200h.png" className="home-image09" />
+          <img alt="image" src="/playground_assets/1rose.png" className="home-image09" />
           <h1>經典之花：紅玫瑰</h1>
           <span className="home-text26">
             <span>以花為貌、以鳥為聲</span>
